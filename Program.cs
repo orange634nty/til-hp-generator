1ba2ff61b1d1cea62d3c1014a0f5d25d3f66a1af
orange634nty <orange634nty@gmail.com>
2019-04-10 23:07:50 +0900

    initial commit.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace til_hp_generator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
