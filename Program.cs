e357db49ba05ed3934e688e716508682d710daa3
orange634nty <orange634nty@gmail.com>
2019-05-02 00:06:49 +0900

    use micro bath framework.

--- Program.cs
+++ Program.cs
@@ -3,12 +3,21 @@ using System.IO;
 using RazorLight;
 using System.Threading.Tasks;
 using Markdig;
+using MicroBatchFramework;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 
 namespace til_hp_generator
 {
-    class Program
+    /// <summary>
+    /// Generate Hp from Til repo
+    /// </summary>
+    class TilHpGenerator : BatchBase
     {
-        public static async Task Main(string[] args)
+        /// <summary>
+        /// Build
+        /// </summary>
+        public async Task Build()
         {
             string mkText = File.ReadAllText(Directory.GetCurrentDirectory() + "/sampleMk/blog.mk");
             var articleText = Markdown.ToHtml(mkText);
@@ -18,6 +27,21 @@ namespace til_hp_generator
               .Build();
             string result = await engine.CompileRenderAsync("tpl/index.cshtml", new { Title = "test title", ArticleText = articleText });
             Console.WriteLine(result);
+        }
+    }
+
+    /// <summary>
+    /// Entry Point Class
+    /// </summary>
+    class Program
+    {
+        /// <summary>
+        /// Entry Point
+        /// </summary>
+        /// <params name="args">Command Line args</params>
+        static async Task Main(string[] args)
+        {
+            await BatchHost.CreateDefaultBuilder().RunBatchEngineAsync<TilHpGenerator>(args);
         }
     }
 }
